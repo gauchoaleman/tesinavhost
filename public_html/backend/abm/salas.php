884dfcd14322e7adea3703e93856fb9e43a3fe71
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2018-10-31 21:59:07 -0300

    Testing atom

---
+++ public_html/backend/abm/salas.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">    
+<?php include "include/inc_head.php";?>
+
+  <body>
+
+    <div class="container">
+
+<?php include "include/inc_menu.php"; 
+	  
+	  if( isset($_GET["mensaje"]) ) echo $_GET["mensaje"];
+	  
+	  if( isset($_GET["accion"]))
+	  {
+	  switch( $_GET["accion"]){
+		case "agregarSala":
+			include "include/inc_formularioAgregarSala.php";
+			break;
+		case "borrarSala":
+			borrarSala($_GET["idAlquiler"]);
+			include "include/inc_listadoSalas.php";
+			break;
+		case "modificarSala":
+			if( $_POST ){
+				include "include/inc_listadoSalas.php";
+			}
+			else
+				include "include/inc_formularioModificarSala.php";
+			break;
+		case "listadoSalas":
+		default:
+			include "include/inc_listadoSalas.php";
+			break;
+	  }
+	  }
+	  else
+	  {
+		  include "include/inc_listadoSalas.php";
+	  }
+?>
+      <!-- Site footer -->
+      <footer class="footer">
+        <p></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+	
+  </body>
+</html>
+<?php 
+function borrarSala($Salas_Id){
+	 global $SqlLink;
+	 $query = "DELETE FROM Salas WHERE Salas_Id=$Salas_Id;";
+	 mysqli_query($SqlLink,$query);
+	 $query = "DELETE FROM recursoalquiler WHERE idAlquiler=$idAlquiler;";
+	 mysqli_query($SqlLink,$query);
+}?>
