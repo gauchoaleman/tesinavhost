48ace31f50fe6a9f59d1a3bb614a38237f5097f8
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2018-11-26 22:49:35 -0300

    limpieza

--- public_html/frontend/include/inc_listaEventos.php
+++ public_html/frontend/include/inc_listaEventos.php
@@ -1,19 +1,20 @@
 <?php
-$ListaEventosquery = "SELECT s.Calle as SalaCalle,s.AlturaCalle as SalaAlturaCalle,e.Fecha as FechaEvento,e.Hora as HoraEvento,e.Nombre as NombreEvento,e.Descripcion as DescripcionEvento,s.Descripcion as DescripcionSala FROM Eventos e,Salas s WHERE s.Salas_Id=e.Salas_Id";
+$ListaEventosquery = "SELECT s.Calle as SalaCalle,s.AlturaCalle as SalaAlturaCalle,e.Fecha as FechaEvento,e.Hora as HoraEvento,e.Nombre as NombreEvento,e.Descripcion as DescripcionEvento,s.Descripcion as DescripcionSala,e.Eventos_Id as Eventos_Id FROM Eventos e,Salas s WHERE s.Salas_Id=e.Salas_Id";
 $Ro = $SqlLink->query($ListaEventosquery);
-  //echo $ListaEventosquery;
+//echo $ListaEventosquery;
 ?>
 
 
 <table border=1>
   <?php while($ListaEventosobj = $Ro->fetch_object()){
     $Evento = $ListaEventosobj->NombreEvento;
+    $Eventos_Id = $ListaEventosobj->Eventos_Id;
     $DescripcionSala = $ListaEventosobj->DescripcionSala;
     $Fecha = date("j/m/Y",strtotime($ListaEventosobj->FechaEvento));
     $Hora=date("H:i",strtotime($ListaEventosobj->HoraEvento));
     $Direccion = $ListaEventosobj->SalaCalle." ".$ListaEventosobj->SalaAlturaCalle;
     echo "<tr><td width='600'>";
-    echo "$Evento / ";
+    echo "<a href='mostrarEvento.php?Eventos_Id=$Eventos_Id'>$Evento</a> / ";
     //  echo $ListaEventosobj->DescripcionEvento;
 
     echo "Sala: $DescripcionSala / ";
---
+++ public_html/frontend/mostrarEvento.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php include "include/inc_head.php"; ?>
+  </head>
+
+  <body>
+    <div class="container">
+
+<?
+extract($_GET);
+echo "<table border=1 width='400'><tr><td>";
+
+$ImpEntquery = "SELECT ent.Nro as Nro,s.Calle as SalaCalle,s.AlturaCalle as SalaAlturaCalle,";
+$ImpEntquery .= "e.Fecha as FechaEvento,e.Hora as HoraEvento,e.Nombre as NombreEvento,";
+$ImpEntquery .= "e.Descripcion as DescripcionEvento,s.Descripcion as DescripcionSala FROM Eventos e,Salas s,Entradas ent WHERE e.Eventos_Id=$Eventos_Id AND s.Salas_Id=e.Salas_Id AND ent.Eventos_Id=e.Eventos_Id";
+//echo $ImpEntquery;
+$Ro = $SqlLink->query($ImpEntquery);
+//echo $ImpEntquery;
+//print_r($Ro);
+$obj = $Ro->fetch_object();
+
+$Nro=$obj->Nro;
+$Evento = $obj->NombreEvento;
+$DescripcionEvento = $obj->DescripcionEvento;
+$DescripcionSala = $obj->DescripcionSala;
+$Fecha = date("j/m/Y",strtotime($obj->FechaEvento));
+$Hora=date("H:i",strtotime($obj->HoraEvento));
+$Direccion = $obj->SalaCalle." ".$obj->SalaAlturaCalle;
+
+echo "<b>$Evento</b><br>";
+echo "$DescripcionEvento<br>";
+
+echo "Sala: $DescripcionSala<br>";
+echo "Fecha: $Fecha<br>";
+echo "Hora: $Hora<br>";
+echo "Dirección: $Direccion<br>";
+echo "</td></tr>";
+echo "</table>";
+?>
+
+
+      <footer class="footer">
+        <p></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
